be4975b2fd1cbf585486474961677a6b9a8abdd0
sues-lee <859518963@qq.com>
2014-10-04 18:33:04 +0800

    Create the Respo

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Temperature
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfirm.Location = new System.Drawing.Point(327, 296);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 0;
+            this.btnConfirm.Text = "查找";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(286, 307);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(427, 296);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnConfirm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string pathIn = @"D:\z.txt";
+        string pathOut = @"D:\x.txt";
+        readonly string pointCode = "58345";    //气象站号
+        readonly string noUse1 = "32766";       //两个无数据标识
+        readonly string noUse2 = "32744";
+        readonly string pattern = @"\s+";                 //匹配空格
+        readonly string patternYear = @"[1-2][0-9]{3}";                 //匹配年份
+        Regex regYear;
+        DataTable dtResult;
+        DataRow drTemp ;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化datatable
+            ClearData();
+
+            regYear = new Regex(patternYear);     //初始化年份匹配
+            //初始化openFileDialog
+            openFileDialog1.Filter = "txt文件(*.txt)|*.txt";
+            saveFileDialog1.Filter = "txt文件(*.txt)|*.txt";
+        }
+
+        private void ClearData()
+        {
+            dtResult = new DataTable();
+            dtResult.Columns.Add("year");
+            dtResult.Columns.Add("month");
+            dtResult.Columns.Add("temp");
+            drTemp = dtResult.NewRow();
+        }
+
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            pathIn = openFileDialog1.FileName;
+            FileStream fs = new FileStream(pathIn, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            
+            double tempTempeature = 0;
+            int tempDay = 0;  //除去无效温度剩下的天数，用来求平均温度
+            int i=0;        //记录当前行数
+           
+            string info; 
+            Regex regex = new Regex(pattern);
+            try
+            {
+                while ((info = sr.ReadLine()) != null)
+                {
+                    string[] tempStr = regex.Split(info);
+                    int t = 0;          //用来找当前月份
+                    foreach (var item in tempStr)
+                    {
+                        if (item != "")     //提取每条数据
+                        {
+                            if (t == 2)
+                            {
+                                //月份的index为2
+                                drTemp["month"] = item;
+                            }
+                            else
+                                if (item == pointCode)
+                                {
+                                    continue;   //查到气象站号时跳过
+                                }
+                                else if (item == noUse1 || item == noUse2)
+                                {
+                                    //无数据时
+                                }
+                                else if (regYear.IsMatch(item))
+                                {
+                                    //判断是否为年份
+                                    drTemp["year"] = item;
+                                }
+                                else
+                                {
+                                    //剩余情况都为有效温度数据
+                                    tempDay++;
+                                    tempTempeature += Convert.ToDouble(item) * 0.1;
+                                }
+                        }
+                        t++;
+                    }
+                    tempTempeature = tempTempeature / tempDay;
+                    drTemp["temp"] = tempTempeature;  //保存平均气温
+                    dtResult.Rows.Add(drTemp);
+                    i++;
+                    tempDay = 0;
+                    tempTempeature = 0;
+                    drTemp = dtResult.NewRow();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("格式错误！");
+                sr.Close();     //关闭连接
+                ClearData();    //清空数据
+                return;
+            }
+            label1.Text = "当前路径：" + pathIn;
+            sr.Close(); //关闭连接
+            dataGridView1.DataSource = dtResult;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource == null)
+            {
+                MessageBox.Show("请导入文件后再输出！");
+                return;
+            }
+            if (saveFileDialog1.ShowDialog()!= System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            pathOut = saveFileDialog1.FileName;
+            StringBuilder result = new StringBuilder();
+            result.Append("=================统计结果==================\r\n");
+            foreach (DataRow item in dtResult.Rows)
+	            {
+                     for (int i = 0; i < item.ItemArray.Length; i++)  
+                        {  
+                            result.Append(item[i].ToString() + "\t");  
+                        }
+                     result.Append("\r\n"); 
+	            }
+            FileStream fs = new FileStream(pathOut, FileMode.Create, FileAccess.Write);  
+            byte[] s = System.Text.Encoding.UTF8.GetBytes(result.ToString());
+            fs.Write(s, 0, s.Length);
+            System.Diagnostics.Process.Start("notepad", pathOut);
+            fs.Close();
+        }
+
+       
+    }
+}
